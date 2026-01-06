e9884ac44ad6036fa75095f6fc9c25c93b1f0af1
FerdigVibes <100457062+FerdigVibes@users.noreply.github.com>
2026-01-05 18:47:11 -0600

    Update state.js

--- docs/assets/js/state.js
+++ docs/assets/js/state.js
@@ -1,1 +1,19 @@
+export const state = {
+  game: "gold",
+  language: "en",
 
+  time: {
+    hour: 12,
+    period: "day"
+  },
+
+  progress: {
+    caught: new Set(),
+    shiny: new Set()
+  },
+
+  ui: {
+    selectedPokemon: null,
+    expandedMobileRow: null
+  }
+};
