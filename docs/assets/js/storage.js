aeae5bdd1a6c8bfd4b996900bc5ec622e5e54794
FerdigVibes <100457062+FerdigVibes@users.noreply.github.com>
2026-01-05 18:48:30 -0600

    Update storage.js

--- docs/assets/js/storage.js
+++ docs/assets/js/storage.js
@@ -1,1 +1,14 @@
+import { state } from "./state.js";
+import { getTimePeriod } from "./time.js";
 
+export function loadState() {
+  const hour = localStorage.getItem("timeHour");
+  if (hour !== null) {
+    state.time.hour = Number(hour);
+    state.time.period = getTimePeriod(state.time.hour);
+  }
+}
+
+export function saveTime(hour) {
+  localStorage.setItem("timeHour", hour);
+}
